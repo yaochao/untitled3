0bb0560d53849f21c22449dd67e6e5eae32fe756
yaochao <yaochao365@qq.com>
2017-12-27 11:50:14 +0800

    fix

--- csv_demo/xueya_demo.py
+++ csv_demo/xueya_demo.py
@@ -6,7 +6,6 @@
 # 挑选出csv中含有血压的列表
 
 import csv
-import math
 
 items = []
 with open('诊疗无结果请求.csv', encoding='utf-8') as f:
@@ -14,7 +13,7 @@ with open('诊疗无结果请求.csv', encoding='utf-8') as f:
     headers = next(f_csv)
     f_csv = list(f_csv)
     for i in range(0, len(f_csv), 7):
-        item = f_csv[i: i+7]
+        item = f_csv[i: i + 7]
         for ii in item[5][3:]:
             if '血压' in ii:
                 items.append(item)
@@ -25,5 +24,3 @@ with open('诊疗无结果请求_血压.csv', mode='w', encoding='utf-8') as f:
     f_csv.writerow(['id', '人员', '', '字段名'])
     for item in items:
         f_csv.writerows(item)
-
-
