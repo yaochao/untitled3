68b059e5acb5fe6ad724f5b8bc23d9f01d73919b
yaochao <yaochao365@qq.com>
2018-01-17 11:57:30 +0800

    fix

--- nlp_workspace/gensim_demo/haodf_demo.py
+++ nlp_workspace/gensim_demo/haodf_demo.py
@@ -21,11 +21,11 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 base_path = '/Users/yaochao/python/datasets/'
 userdict_path = base_path + 'user_dicts/online_and_icd_and_mesh.txt'
 stopwords_path = base_path + 'user_dicts/stopwords5.txt'
-file_path = base_path + 'haodf_chats_detail_1000W_pre.csv.word2vec_model'
-file_path2 = base_path + 'haodf_chats_detail_1000W_pre.csv.w2v_model'
-file_path3 = base_path + 'downloads/cn.cbow.bin'
-file_path4 = base_path + 'downloads/cn.skipgram.bin'
-file_path5 = base_path + 'downloads/Twitter_word_embeddings_CBOW.csv'
+file_path_100 = base_path + 'haodf_chats_detail_100W_pre.csv.word2vec_model'
+file_path_1000 = base_path + 'haodf_chats_detail_1000W_pre.csv.word2vec_model'
+file_path_1000_stopwords = base_path + 'haodf_chats_detail_1000W_pre.csv.w2v_model'
+file_path_baike_cbow = base_path + 'downloads/cn.cbow.bin'
+file_path_baike_skipgram = base_path + 'downloads/cn.skipgram.bin'
 # file_path = base_path + 'abaike_10000.word2vec_model'
 mysql_config = {
     'host': '127.0.0.1',
@@ -238,13 +238,17 @@ def map_online_to_icd():
 
 def use_model():
     # load the original Google word2vec model, use the KeyedVectors.load_word2vec_format()
-    # model_cbow = gensim.models.KeyedVectors.load_word2vec_format(fname=file_path3, binary=True, encoding='utf-8', unicode_errors='ignore')
-    # model_skipgram = gensim.models.KeyedVectors.load_word2vec_format(fname=file_path4, binary=True, encoding='utf-8', unicode_errors='ignore')
-    model = gensim.models.KeyedVectors.load_word2vec_format(fname=file_path5, binary=False, encoding='utf-8', unicode_errors='ignore')
-    # model = gensim.models.Word2Vec.load(file_path3)
+    model_cbow = gensim.models.KeyedVectors.load_word2vec_format(fname=file_path_baike_cbow, binary=True, encoding='utf-8', unicode_errors='ignore')
+    # model_skipgram = gensim.models.KeyedVectors.load_word2vec_format(fname=file_path_baike_skipgram, binary=True, encoding='utf-8', unicode_errors='ignore')
+    model_100 = gensim.models.Word2Vec.load(file_path_100)
+    model_1000 = gensim.models.Word2Vec.load(file_path_1000)
+    model_1000_stopwords = gensim.models.Word2Vec.load(file_path_1000_stopwords)
     # print('你好' in model)
-    word = 'hello'
-    print(model.wv.most_similar(word, topn=5, ))
+    word = '近视'
+    print('100     ', model_100.wv.most_similar(word, topn=5))
+    print('1000    ', model_1000.wv.most_similar(word, topn=5))
+    print('1000sw  ', model_1000_stopwords.wv.most_similar(word, topn=5))
+    print('baike   ', model_cbow.wv.most_similar(word, topn=5))
     # print(model.wv.similarity('中国', '北京'))
     # print(cos_similarity(model['中国'], model['北京']))
 
