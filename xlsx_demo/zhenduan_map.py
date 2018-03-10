54d418a577565225efb0a5e23aa6873b5e565bb4
yaochao <yaochao365@qq.com>
2018-03-10 08:45:58 +0800

    fix

--- xlsx_demo/zhenduan_map.py
+++ xlsx_demo/zhenduan_map.py
@@ -10,6 +10,23 @@ import re
 f = '/Users/yaochao/Downloads/吉林_his_诊断.xlsx'
 symbols = ['.', '-', '和', '，', ',', '、', '/', ' ', '或', '[', '伴']
 
+symbols_re = [
+    r'\w+(-{1,7})\w+',
+    r'\w+(，)[^其他的]\w+',  # 继发性肺动脉高压，其他的
+    r'\w+(\[\w+\]$)\w+',  # 过敏性鼻炎[变应性鼻炎]        急性鼻咽炎［感冒］      变应性［过敏性］紫癜
+    r'\w+(、)\w+',  # 高血压病、脑供血不足
+    r'\w+(/)\w+',  # 高血压/冠心病
+    r'\w+( {1,})\w+',  # 高血压 脑梗
+    r'\w+(,)[^其他的]\w+',  # 高血压病,脑梗
+    r'\w+(\.)\w+',  # 不规则流血.尿路感染
+    r'\w+(。)\w+',  # 盆腔炎。膀胱炎
+    r'\w+(和)\w+',  # 生殖器和泌尿生殖道的疱疹病毒感染
+    r'\w+(伴有)\w+',
+    r'\w+(伴)\w+',
+    r'\w+(或)\w+',
+    r'\w+[^合](并)[^发证]\w+',
+]
+
 
 def get_all_values(f, sheet, index):
     hook = xlrd.open_workbook(filename=f)
@@ -18,38 +35,86 @@ def get_all_values(f, sheet, index):
     return col_values
 
 
-def get_all_symbol():
-    r = []
-    col_values = get_all_values(f, 0, 0)
-    print(len(col_values))
-    for i in col_values:
-        i = str(i)
-        i = i.strip()
-        for s in symbols:
-            if s in i:
-                r.append(i)
+col_values = get_all_values(f, 0, 0)
 
+
+def get_all_split_name():
+    '''
+    将所有的name进行分割
+    :return:
+    '''
+    r = []
+    for i in col_values:
+        i = str(i).strip()
+        names = get_split_name(i)
+        if names:
+            r.append(names)
+            names.insert(0, i)
+        else:
+            r.append([i])
     return r
 
 
-def split_N_words():
-    r = get_all_symbol()
-    for idx, i in enumerate(r):
-        pattern = r'{}+'.format('-')
-        print(re.sub(pattern, 'yaochao', i))
+def get_split_name(name):
+    '''
+    找到这个name的一种分割形式
+    :param name:
+    :return:
+    '''
+    for s in symbols_re:
+        result = re.findall(s, name)
+        if result:
+            flag = result[0]
+            names = name.split(flag)
+            names.insert(0, flag)
+            return names
+
+
+def get_all_split_name2(name):
+    '''
+    单独的某个分割符的所有结果
+    :param name:
+    :return:
+    '''
+    result = []
+    for s in symbols_re:
+        for i in col_values:
+            result = re.findall(s, name)
+            if result:
+                flag = result[0]
+                names = name.split(flag)
+                names.insert(0, flag)
+                names.insert(0, i)
+                result.append(names)
+                break
+    return result
 
 
 def write_to_xls(out_path='out.xls'):
+    '''
+    写到xls中
+    :param out_path: 输出路径
+    :return:
+    '''
     book = xlwt.Workbook(encoding='utf8')
     sheet1 = book.add_sheet(sheetname='sheet1')
-    r = get_all_symbol()
-    for idx, i in enumerate(r):
-        sheet1.write(idx, 0, i)
+    r = get_all_split_name()
+    # 写表头
+    sheet1.write(0, 0, '原诊断名称')
+    sheet1.write(0, 1, '分割符')
+    sheet1.write(0, 2, '新1')
+    sheet1.write(0, 3, '新2')
+    sheet1.write(0, 4, '新3')
+    sheet1.write(0, 5, '新4')
+    # 循环写表内容
+    for row, i in enumerate(r):
+        for column, ii in enumerate(i):
+            sheet1.write(row + 1, column, ii)
     book.save(out_path)
 
 
 def main():
-    split_N_words()
+    write_to_xls()
 
 
 if __name__ == '__main__':
