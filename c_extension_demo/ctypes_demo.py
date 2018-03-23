c8c9b443053f11581d4a8bf81a05f38cb186d5de
yaochao <yaochao365@qq.com>
2018-03-24 00:57:26 +0800

    fix

--- c_extension_demo/ctypes_demo.py
+++ c_extension_demo/ctypes_demo.py
@@ -9,4 +9,4 @@ hello_lib = ctypes.cdll.LoadLibrary('hello.so')
 
 hello = hello_lib.hello
 hello('Hello World')
-# TODO：将Python字符串传递给C，是有问题的。上面那个只能打印出一个H
+# TODO：将Python字符串传递给C，是有问题的。上面那个只能打印出一个H（python2没事，python3有问题）
