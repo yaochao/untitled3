f2b4e5f0862283459bf53f18352740ff2a055512
yaochao <yaochao365@qq.com>
2018-05-23 15:04:56 +0800

    fix

--- qiao_2_ksc/tmp.py
+++ qiao_2_ksc/tmp.py
@@ -1,20 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*- 
 # Created by yaochao at 2018/5/15
-
-
-from qiao_2_ksc import MYSQL_CONF_DEV, MYSQL_CONF_LOCAL
+import pymongo
 import pymysql
 
-connect = pymysql.connect(**MYSQL_CONF_DEV)
-cursor = connect.cursor()
-cursor2 = connect.cursor()
+MYSQL_CONF_LOCAL = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'passwd': 'toor',
+    'db': 'work2',
+    'charset': 'utf8',
+    'cursorclass': pymysql.cursors.DictCursor,
+}
+
+MYSQL_CONF_DEV = {
+    'host': '127.0.0.1',
+    'port': 8891,
+    'user': 'phstest',
+    'passwd': 'phstest@20161205',
+    'db': 'g_lb',
+    'charset': 'utf8',
+    'cursorclass': pymysql.cursors.DictCursor,
+}
+
+MYSQL_CONF_DEV2 = {
+    'host': '172.31.48.2',
+    'port': 8091,
+    'user': 'phstest',
+    'passwd': 'phstest@20161205',
+    'db': 'g_lb',
+    'charset': 'utf8',
+    'cursorclass': pymysql.cursors.DictCursor,
+}
+
+MONGO_CONF = {
+    'host': '127.0.0.1',
+    'port': 27017,
+}
+
+MONGO_CONF2 = {
+    'host': '127.0.0.1',
+    'port': 27018,
+}
+
 
 def update_sjzq_ry():
     '''
     把创建的人员更新到个人档案对应的建档人员和责任医生
     :return:
     '''
+    connect = pymysql.connect(**MYSQL_CONF_DEV)
+    cursor = connect.cursor()
     sql = 'SELECT * FROM sjzq_ry'
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -34,6 +71,8 @@ def create_ry():
     创建人员
     :return:
     '''
+    connect = pymysql.connect(**MYSQL_CONF_DEV)
+    cursor = connect.cursor()
     # sql = 'SELECT sqbm, zrys FROM sjzq_grda where zrysid is null and zrys is not null'
     sql = 'SELECT sqbm, jdrymc FROM sjzq_grda where jdryid is null and jdrymc is not null'
     cursor.execute(sql)
@@ -47,7 +86,7 @@ def create_ry():
     for index, i in enumerate(all_set):
         sqbm = i[0]
         zrys = i[1]
-        bm = sqbm + 'B'+str(index)
+        bm = sqbm + 'B' + str(index)
         state = 1
         prj = 1
         print(index)
@@ -62,13 +101,15 @@ def change_sqbm_add_1522():
     把个人档案里面机构的编码，开头为22的，加上1522.
     :return:
     '''
+    connect = pymysql.connect(**MYSQL_CONF_DEV)
+    cursor = connect.cursor()
     # select sqbm start with '22'
     sql = 'SELECT DISTINCT sqbm FROM sjzq_grda WHERE sqbm LIKE "22%"'
     cursor.execute(sql)
-    result = cursor.fetchall()
+    result = cursor.fetchallm()
     result = [i['sqbm'] for i in result]
 
-    #update
+    # update
     sql2 = 'UPDATE sjzq_grda SET sqbm=%s WHERE sqbm=%s'
     for i in result:
         print(i)
@@ -78,8 +119,27 @@ def change_sqbm_add_1522():
     connect.close()
 
 
-
+def fix_fjh():
+    '''
+    错误原因：本该为0的肺结核，弄错为1了，找出这些弄错的，并把isupdate改为-2
+    :return:
+    '''
+    # mysql
+    connect = pymysql.connect(**MYSQL_CONF_DEV)
+    cursor = connect.cursor()
+    # mongo
+    client = pymongo.MongoClient(**MONGO_CONF2)
+    db = client['datasets']
+    collection = db['xam_person_list_518']
+    result = collection.find({"FEA_PTB": {"$eq": 0}})
+    for i in result:
+        ehrId = i['EHR_ID']
+        sql = 'UPDATE sjzq_grda SET isupdate=-2,isfjh=0 where bm=%s and isfjh=1'
+        cursor.execute(sql, (ehrId))
+        print(ehrId)
+        connect.commit()
+    connect.close()
 
 
 if __name__ == '__main__':
-    change_sqbm_add_1522()
+    fix_fjh()
--- qiao_2_ksc/tmp2.py
+++ qiao_2_ksc/tmp2.py
@@ -1,56 +1,376 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*- 
 # Created by yaochao at 2018/5/21
-
+import pymongo
 import requests
+import pymysql
+from pymongo.errors import DuplicateKeyError
+
+MONGO_CONF2 = {
+    'host': '127.0.0.1',
+    'port': 27018,
+}
+
+MYSQL_CONF_DEV = {
+    'host': '127.0.0.1',
+    'port': 8891,
+    'user': 'phstest',
+    'passwd': 'phstest@20161205',
+    'db': 'g_lb',
+    'charset': 'utf8',
+    'cursorclass': pymysql.cursors.DictCursor,
+}
+
+# mysql init
+connect = pymysql.connect(**MYSQL_CONF_DEV)
+
+# mongodb init
+client = pymongo.MongoClient(**MONGO_CONF2)
+db = client['datasets']
+xam_et_list_521 = db['xam_et_list_521']
+xam_ycf_list_521 = db['xam_jsb_list_521']
+xam_lnr_list_522 = db['xam_lnr_list_522']
+xam_fjh_list_521 = db['xam_fjh_list_521']
+xam_tnb_list_521 = db['xam_tnb_list_521']
+xam_gxy_list_521 = db['xam_gxy_list_521']
 
 
-def diff_lnr():
+def login(username, password):
     '''
-    对比老年人不同的数据
-    :return:
+    启奥系统 - 登录接口
+    :param username:
+    :param password:
+    :return: requests.session object with login cookie info
     '''
-    with open('/Users/yaochao/test/lnr.txt', 'r') as f:
-        ehrids = f.readlines()
-        ehrids = [x.strip() for x in ehrids]
-        print(ehrids)
-
-
-def send_request():
-    # 启奥系统 - 人员列表
-    # POST http://jkda.xamwjw.gov.cn/orguser/queryUserByParams.action
-
     try:
-        response = requests.post(
-            url="http://jkda.xamwjw.gov.cn/orguser/queryUserByParams.action",
+        s = requests.session()
+        response = s.post(
+            url="http://jkda.xamwjw.gov.cn/j_spring_security_check",
             headers={
                 "Origin": "http://jkda.xamwjw.gov.cn",
-                "Cookie": "JSESSIONID=4EFDB50B89669BC2ED010CF8461C43E3.tomcatA1; Shinow=suliyun; shinow_is_max=0",
-                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
-                "Referer": "http://jkda.xamwjw.gov.cn/main.action",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Referer": "http://jkda.xamwjw.gov.cn/login.action",
                 "Host": "jkda.xamwjw.gov.cn",
                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36",
                 "X-Requested-With": "XMLHttpRequest",
             },
             data={
-                "orgUserBean.sex": "",
-                "page": "1",
-                "orgUserBean.orgperStatus": "",
-                "start": "0",
-                "orgUserBean.technicTitle": "",
-                "orgUserBean.positionCode": "",
-                "limit": "10000",
                 "jsonResult": "true",
-                "orgUserBean.empName": "",
-                "orgUserBean.idNo": "",
-                "orgUserBean.deptId": "",
+                "j_username": username,
+                "j_password": password,
+            },
+        )
+        r = response.json()
+        print(r)
+        if r['loginResult']['result']:
+            return s
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+
+
+def get_et_list(session):
+    # 启奥系统 - 儿童列表
+
+    try:
+        response = session.post(
+            url="http://jkda.xamwjw.gov.cn/grid/page/query.action",
+            params={
+                "gridCode": "ph_cld_grid",
+                "paging": "1",
+                "_dc": "1526902971795",
+            },
+            headers={
+                "Origin": "http://jkda.xamwjw.gov.cn",
+                "Accept": "*/*",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Referer": "http://jkda.xamwjw.gov.cn/main.action",
+                "Host": "jkda.xamwjw.gov.cn",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+                "Accept-Language": "zh_CN",
+                "X-Requested-With": "XMLHttpRequest",
+            },
+            data={
+                "start": "0",
+                "parameter.ehrid": "",
+                "parameter.fmTel": "",
+                "jsonResult": "true",
+                "parameter.p_name": "",
+                "parameter.dsn": "",
+                "parameter.filetype": "",
+                "parameter.fmName": "",
+                "parameter.R_org": "001001002",
+                "limit": "1000000",
+                "page": "1",
+                "parameter.id_no": "",
             },
         )
         result = response.json()
+        print('儿童total:%s' % result['resultData']['totalProperty'])
+        for i in result['resultData']['pageData']:
+            ehrId = i['EHR_ID']
+            i['_id'] = ehrId
+            try:
+                xam_et_list_521.insert(i)
+            except DuplicateKeyError as e:
+                print(e)
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+
+
+def get_ycf_list(session):
+    # 启奥系统 - 孕产妇列表
+
+    try:
+        response = session.post(
+            url="http://jkda.xamwjw.gov.cn/grid/page/query.action",
+            params={
+                "gridCode": "ph_pm_grid",
+                "paging": "1",
+                "_dc": "1526904782437",
+            },
+            headers={
+                "Origin": "http://jkda.xamwjw.gov.cn",
+                "Accept": "*/*",
+                "Cookie": "JSESSIONID=32A77C27BF593F9E259AF16C40B01DA3.tomcatA1; Shinow=xingjinhua; shinow_is_max=0",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Referer": "http://jkda.xamwjw.gov.cn/main.action",
+                "Host": "jkda.xamwjw.gov.cn",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+                "Accept-Language": "zh_CN",
+                "X-Requested-With": "XMLHttpRequest",
+            },
+            data={
+                "parameter.enddate": "",
+                "parameter.ehrid": "",
+                "start": "0",
+                "jsonResult": "true",
+                "parameter.p_name": "",
+                "parameter.org": "001001002",
+                "parameter.dsn": "",
+                "parameter.jieduan": "",
+                "parameter.startdate": "",
+                "parameter.filetype": "",
+                "limit": "1000000",
+                "page": "1",
+                "parameter.id_no": "",
+            },
+        )
+        result = response.json()
+        print('孕产妇total:%s' % result['resultData']['totalProperty'])
+        for i in result['resultData']['pageData']:
+            ehrId = i['EHR_ID']
+            i['_id'] = ehrId
+            try:
+                xam_ycf_list_521.insert(i)
+            except DuplicateKeyError as e:
+                print(e)
 
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
 
 
+def get_lnr_list():
+    # 启奥系统 - 老年人列表
+
+    try:
+        response = requests.post(
+            url="http://jkda.xamwjw.gov.cn/grid/page/query.action",
+            params={
+                "gridCode": "ph_op_grid",
+                "paging": "1",
+                "_dc": "1526975244060",
+            },
+            headers={
+                "Origin": "http://jkda.xamwjw.gov.cn",
+                "Accept": "*/*",
+                "Cookie": "JSESSIONID=FA6DA32EE0ED53F8EB16AFD6404A6A42.tomcatA1; Shinow=xingjinhua; shinow_is_max=0",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Referer": "http://jkda.xamwjw.gov.cn/main.action",
+                "Host": "jkda.xamwjw.gov.cn",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+                "Accept-Language": "zh_CN",
+                "X-Requested-With": "XMLHttpRequest",
+            },
+            data={
+                "parameter.ehrstatus": "0",
+                "parameter.ehrid": "",
+                "start": "0",
+                "parameter.year_service": "0",
+                "parameter.p_name": "",
+                "parameter.year": "2018",
+                "parameter.org": "001001002",
+                "parameter.start_date": "",
+                "parameter.end_date": "",
+                "parameter.dsn": "",
+                "parameter.filetype": "0",
+                "jsonResult": "true",
+                "limit": "1000000",
+                "page": "1",
+                "parameter.id_no": "",
+            },
+        )
+        result = response.json()
+        print('老年人total:%s' % result['resultData']['totalProperty'])
+        for i in result['resultData']['pageData']:
+            ehrId = i['EHR_ID']
+            i['_id'] = ehrId
+            print(ehrId)
+            try:
+                xam_lnr_list_522.insert(i)
+            except DuplicateKeyError as e:
+                print(e)
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+
+
+def get_gxy_list(session):
+    # 启奥系统 - 高血压列表
+
+    try:
+        response = session.post(
+            url="http://jkda.xamwjw.gov.cn/grid/page/query.action",
+            params={
+                "gridCode": "ph_hyp_grid",
+                "paging": "1",
+                "_dc": "1526905119186",
+            },
+            headers={
+                "Origin": "http://jkda.xamwjw.gov.cn",
+                "Accept": "*/*",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Referer": "http://jkda.xamwjw.gov.cn/main.action",
+                "Host": "jkda.xamwjw.gov.cn",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+                "Accept-Language": "zh_CN",
+                "X-Requested-With": "XMLHttpRequest",
+            },
+            data={
+                "start": "0",
+                "parameter.ehrid": "",
+                "jsonResult": "true",
+                "parameter.year_service": "0",
+                "parameter.p_name": "",
+                "parameter.year": "2018",
+                "parameter.start_date": "",
+                "parameter.end_date": "",
+                "parameter.dsn": "",
+                "parameter.filetype": "0",
+                "parameter.R_org": "001001002",
+                "parameter.age": "35",
+                "limit": "1000000",
+                "page": "1",
+                "parameter.id_no": "",
+            },
+        )
+        result = response.json()
+        print('高血压total:%s' % result['resultData']['totalProperty'])
+        for i in result['resultData']['pageData']:
+            ehrId = i['EHR_ID']
+            i['_id'] = ehrId
+            try:
+                xam_gxy_list_521.insert(i)
+            except DuplicateKeyError as e:
+                print(e)
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+
+
+def fix_et():
+    cursor = connect.cursor()
+    cursor2 = xam_et_list_521.find()
+    sql = 'SELECT iset FROM sjzq_grda where bm=%s'
+    sql2 = 'UPDATE sjzq_grda SET iset=1,isupdate=-4 where bm=%s'
+    for i in cursor2:
+        ehrId = i['_id']
+        cursor.execute(sql, (ehrId))
+        result = cursor.fetchone()
+        if result and result['iset'] == 0:
+            cursor.execute(sql2, (ehrId))
+            connect.commit()
+
+
+def fix_gxy():
+    cursor = connect.cursor()
+    sql = 'SELECT bm FROM sjzq_grda where isgxy=1'
+    sql2 = 'UPDATE sjzq_grda SET isgxy=0,isupdate=-3 where bm=%s'
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for i in result:
+        bm = i['bm']
+        r = xam_gxy_list_521.find({'_id': bm}).count()
+        if not r:
+            cursor.execute(sql2, bm)
+            connect.commit()
+            print(r)
+
+
+def fix_tnb():
+    cursor = connect.cursor()
+    cursor2 = xam_tnb_list_521.find()
+    sql = 'SELECT bm FROM sjzq_grda where istnb=1'
+    sql2 = 'UPDATE sjzq_grda SET istnb=0,isupdate=-5 where bm=%s'
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for i in result:
+        bm = i['bm']
+        r = xam_tnb_list_521.find({'_id': bm}).count()
+        if not r:
+            print(r)
+
+
+def fix_lnr():
+    cursor = connect.cursor()
+    cursor2 = xam_lnr_list_522.find()
+    sql = 'SELECT islnr FROM sjzq_grda where bm=%s'
+    sql2 = 'UPDATE sjzq_grda SET islnr=1,isupdate=-6 where bm=%s'
+    for i in cursor2:
+        ehrId = i['_id']
+        cursor.execute(sql, (ehrId))
+        result = cursor.fetchone()
+        if result and result['islnr'] == 0:
+            print(ehrId)
+            cursor.execute(sql2, (ehrId))
+            connect.commit()
+
+
+def diff_lnr():
+    cursor = connect.cursor()
+    sql = 'SELECT DISTINCT bm FROM sjzq_grda WHERE isupdate=-6;'
+    sql2 = 'SELECT bm FROM sjzq_grda WHERE isupdate=-6;'
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    result = [x['bm'] for x in result]
+    print(len(result))
+
+    cursor.execute(sql2)
+    result2 = cursor.fetchall()
+    result2 = [x['bm'] for x in result2]
+    print(len(result2))
+    for i in result:
+        result2.remove(i)
+    print(result2)
+
+
+def diff_lnr2():
+    with open('522已插入的老年人.txt') as f:
+        result = f.readlines()
+        result = [x.split('\t')[0] for x in result]
+    with open('老年人档案更新.txt') as f:
+        result2 = f.readlines()
+        result2 = [x.split('bm=\'')[-1].split('\'')[0] for x in result2]
+    not_insert = []
+    for i in result2:
+        if i not in result:
+            not_insert.append(i)
+    print(len(not_insert))
+    with open('522未插入的老年人bm.txt', 'w') as f:
+        f.write('\n'.join(not_insert))
+
+
+def main():
+    session = login('xingjinhua', '123456789')
+    get_gxy_list(session)
+
+
 if __name__ == '__main__':
-    diff_lnr()
+    diff_lnr2()
